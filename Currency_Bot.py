5d1581819393804041e8f172144e5d7aa647ca68
tamatoo <tatamatoo@openviza.com>
2019-10-06 19:22:55 -0700

    This is a fully working version

--- Currency_Bot.py
+++ Currency_Bot.py
@@ -57,13 +57,37 @@ async def on_ready():
 
 @client.event
 async def on_message(context):
+
+    # makeing global functions and lists and variables
+    stop = False
+    admins = ["tamatoo"]
+    def update_user(user):
+        print("updating user")
+        for i in user.stocks:
+            stock = i
+            company = Company.query.filter_by(company_id=stock.company_id).first()
+            stock.value = (company.company_value/len(company.stocks))
+            db.session.commit()
+
+
     if context.content.lower() == "currency_bot!":
-        await context.channel.send("Hello, I am Currency Bot here are some commands: 'Create Account!' to initiate your account, 'stock stats!' for the current prices of the stocks, for your account's information message 'my account', To buy a stock in a company say 'buy (amount of stock) (company name)', and To sell a stock in a company say 'sell (amount of stock) (company name)'")
+        await context.channel.send("Hello, I am Currency Bot here are some commands: 'create_account!' to initiate your account, to see a current list of companies available message 'companies!', for your account's information message 'account!', to buy a stock in a company say 'buy_share! (quantity of shares) (company name)', and to sell a stock in a company say 'sell_share! (quantity of shares) (company name)', to create a company say 'create_company! (company's name)' you must have admin permissions for this command; message an admin to be added to the list of admins, to see a current list of users message 'users!', and to get commands to interact with the database say 'help_database!'")
         print("introduction called")
 
+    elif context.content.lower() == "update_sim!":
+        if context.author.name in admins:
+            print("updating sim by command of: " + context.author.name)
+            await context.channel.send("I am updating the simulation right now this may take awhile, you will not be able to buy or sell stock during this time.")
+            stop = True
+            for i in Company.query.all():
+                for x in i.stocks:
+                    x.value = str(int(i.company_value)/len(i.stocks))
+            await context.channel.send("I have finished updateing the simulation, commands can now be made.")
+            stop = False
+        else:
+            await context.channel.send("I am sorry but you don't have permission to do this, ask an admin to add you to the list of admins.")
 
-    elif context.content.lower() == "create_account!":
-        print(type(context.author.id))
+    elif context.content.lower() == "create_account!" and stop == False:
         print("trying")
         #user check
         if not User.query.filter_by(discord_id=str(context.author.id)).first():
@@ -71,13 +95,13 @@ async def on_message(context):
             db.session.add(user)
             db.session.commit()
             await context.channel.send("Your account was created.")
-            print("account created")
+            print("Succeeded to create account:success")
         else:
-            await context.channel.send("You already have an account. To get out of debt contact Tamatoo.")
-            print("failed to create account")
+            await context.channel.send("You already have an account.")
+            print("failed to create account:alreadyexist")
 
 
-    elif context.content.lower().split(" ")[0] == "account!":
+    elif context.content.lower().split(" ")[0] == "account!" and stop == False:
         if len(context.content.lower().split(" ")) == 2:
             if User.query.filter_by(username=str(context.content.split(" ")[1])).first():
                 await context.channel.send(User.query.filter_by(username=str(context.content[1])).first())
@@ -94,7 +118,7 @@ async def on_message(context):
                 print("failed to show account:noexist")
 
 
-    elif context.content.lower().split(" ")[0] == "change_name!":
+    elif context.content.lower().split(" ")[0] == "change_name!" and stop == False:
         if len(context.content.split(" ")) == 1:
             if User.query.filter_by(discord_id=str(context.author.id)).first():
                 User.query.filter_by(discord_id=str(context.author.id)).first().username = context.author.name
@@ -105,7 +129,7 @@ async def on_message(context):
                 await context.channel.send("Sorry you dont have an account, you can make one with the command 'create_account!'.")
                 print("failed to change the username:noexist")
         else:
-            await context.channel.send("Please check formatting")
+            await context.channel.send("Your formmatting was wrong.")
             print("failed to change the username:formatting")
 
 
@@ -118,18 +142,18 @@ async def on_message(context):
         print("showing all companies at this time")
 
 
-    elif context.content.lower().split(" ")[0] == "create_company!":
+    elif context.content.lower().split(" ")[0] == "create_company!" and stop == False:
         if len(context.content.split(" ")) == 2:
             if not Company.query.filter_by(company_name=context.content.split(" ")[1]).first():
-                if context.author.name == "tamatoo":
+                if context.author.name in admins:
                     company_1 = Company(company_name=context.content.split(" ")[1])
                     db.session.add(company_1)
                     db.session.commit()
-                    await context.channel.send("New company has been made.")
+                    await context.channel.send("A new company has been made.")
                     print("Succeeded to make company:success")
                 else:
                     await context.channel.send("Sorry you don't have permission to do this.")
-                    print("failed to make company:nopermission")
+                    print("failed to make company:notadmin")
             else:
                 await context.channel.send("Sorry there already is a company with that name.")
                 print("failed to make company:alreadyexist")
@@ -138,37 +162,92 @@ async def on_message(context):
             print("failed to make company:formatting")
 
 
-    elif context.content.lower().split(" ")[0] == "buy_stock!":
+    elif context.content.lower().split(" ")[0] == "buy_share!" and stop == False:
         if len(context.content.split(" ")) == 3:
-            quantity = int(context.content.split(" ")[2])
-            if quantity < 10:
+            try:
+                quantity = int(context.content.split(" ")[1])
+            except:
+                await context.channel.send("You need to put a whole number(exp:'3') for the quantity.")
+                print("failed to purchase stock:notquantity")
+            if quantity <= 10:
                 if User.query.filter_by(discord_id=context.author.id).first():
                     user = User.query.filter_by(discord_id=context.author.id).first()
-                    company = Company.query.filter_by(company_name=context.content.split(" ")[1]).first()
+                    company = Company.query.filter_by(company_name=context.content.split(" ")[2]).first()
                     if company:
-                        def makeing_stock():
+                        def making_stock():
                             transaction = "nothing"
                             stock = Stock(value="0", state="sold", user_id=user.id, company_id=company.id)
-                            if int(company.old_stock_number) == 0:
-                                stock_price = 1
-                            else:
-                                stock_price = len(company.stocks)/int(company.old_stock_number)
-                            company.company_value = stock_price*len(company.stocks)
+                            try:
+                                stock_price = len(company.stocks)
+                            except:
+                                stock_price = 0
                             stock.value = str(stock_price)
                             company.old_stock_number = len(company.stocks)
                             if int(user.money) >= stock_price*quantity and int(user.money) != 0:
-                                user.money = str(int(user.money) - int(stock.value))
+                                user.money = str(int(user.money) - stock_price)
+                                transaction = "You bought one share, Company: " + company.company_name + " Price: " + stock.value
                                 db.session.add(stock)
                                 db.session.commit()
+                                company.company_value = stock_price*len(company.stocks)
+                                db.session.commit()
                                 print("Succeeded to purchase stock:success")
-                                transaction = "You bought one stock, good luck."
                                 return transaction
                             else:
                                 transaction = "Sorry you dont have enough money."
                                 print("failed to purchase stock:notenoughmoney")
                                 return transaction
-                        for i in range(0, int(context.content.split(" ")[2])):
-                            await context.channel.send(makeing_stock())
+                        for i in range(0, quantity):
+                            await context.channel.send(making_stock())
+                    else:
+                        await context.channel.send("Sorry but that company does not exist.")
+                        print("failed to purchase stock:nocompany")
+                else:
+                    await context.channel.send("Sorry but you don't have an account, make an account with the 'create_account!' command.")
+                    print("failed to purchase stock:noaccount")
+            else:
+                await context.channel.send("Sorry but the transaction is too big, the maximum is 10.")
+                print("failed to purchase stock:toobig")
+        else:
+            await context.channel.send("Sorry but the formmatting you used is wrong.")
+            print("failed to purchase stock:formatting")
+
+
+    elif context.content.lower().split(" ")[0] == "sell_share!" and stop == False:
+        if len(context.content.split(" ")) == 3:
+            try:
+                quantity = int(context.content.split(" ")[1])
+            except:
+                await context.channel.send("You need to put an whole number(exp:'3') for the quantity.")
+                print("failed to purchase stock:notquantity")
+            if quantity < 10:
+                if User.query.filter_by(discord_id=context.author.id).first():
+                    user = User.query.filter_by(discord_id=context.author.id).first()
+                    company = Company.query.filter_by(company_name=context.content.split(" ")[2]).first()
+                    if company:
+                        def selling_stock():
+                            try:
+                                transaction = "nothing"
+                                #look for the stock
+                                for i in user.stocks:
+                                    if i.company_id == company.id:
+                                        stock = i
+                                        stock.value = str(len(company.stocks)-1)
+                                        company.company_value = str(int(stock.value)*len(company.stocks))
+                                        user.money = str(int(user.money) + int(stock.value))
+                                        db.session.delete(stock)
+                                        db.session.commit()
+                                        break
+                                company.old_stock_number = len(company.stocks)
+                                transaction = "You sold one share, Company: " + company.company_name + " Price: " + stock.value
+                                db.session.commit()
+                                print("Succeeded to sell stock:success")
+                                return transaction
+                            except:
+                                transaction = "Sorry you dont have enough shares."
+                                print("failed to sell stock:notenoughstock")
+                                return transaction
+                        for i in range(0, quantity):
+                            await context.channel.send(selling_stock())
                     else:
                         await context.channel.send("Sorry but that company does not exist.")
                         print("failed to purchase stock:nocompany")
@@ -176,12 +255,84 @@ async def on_message(context):
                     await context.channel.send("Sorry but you don't have an account, make an account with the create_account! command.")
                     print("failed to purchase stock:noaccount")
             else:
-                await context.channel.send("Sorry but the transaction is too big.")
+                await context.channel.send("Sorry but the transaction is too big, there is a max of 10.")
                 print("failed to purchase stock:toobig")
         else:
-            await context.channel.send("Sorry but you need to have the quantity be a number")
+            await context.channel.send("Sorry but the formmatting you used is wrong.")
             print("failed to purchase stock:formatting")
 
 
-client.run("NDg4MDIzNDA0NjE2NjEzOTAz.DndQBQ.V4w8jjvBEVRTD4behMPyPi5GPeE")
-#cd C:\Users\shakeandbakeforever\Discord_Bots
+    #database commmands
+
+
+    elif context.content.lower() == "database_help!":
+        if context.author.name in admins:
+            if context.author.dm_channel:
+                await context.author.dm_channel.send("Here are some database commands: to delete a user say 'delete_user! (the persons username)', to delete a company say 'delete_company (company name)', and to add an admin say 'add_admin! (the persons discord username without the numbers exp:'tamatoo')'")
+                print("Succeeded to help admin:success")
+            else:
+                await context.author.create_dm()
+                await context.author.dm_channel.send("Here are some database commands: to delete a user say 'delete_user! (the persons username)', to delete a company say 'delete_company (company name)', and to add an admin say 'add_admin! (the persons discord username without the numbers exp:'tamatoo')'")
+                print("Succeeded to help admin:success")
+        else:
+            await context.channel.send("Sorry but you are not an admin, message an admin to become an admin.")
+            print("failed to help admin:notadmin")
+
+    elif context.content.lower().split(" ")[0] == "delete_user!":
+        if context.author.name in admins:
+            if len(context.content.lower().split(" ")) == 2:
+                user = User.query.filter_by(username=context.content.split(" ")[1]).first()
+                if user:
+                    for i in user.stocks:
+                        db.session.delete(i)
+                    db.session.delete(user)
+                    db.session.commit()
+                    await context.channel.send("I have deleted that user.")
+                    print("Succeeded to delete user:success")
+                else:
+                    await context.channel.send("Sorry but the user you entered does not exist.")
+                    print("failed to delete user:noexist")
+            else:
+                await context.channel.send("Sorry but the formmatting you used is wrong.")
+                print("failed to delete user:wrongformatt")
+        else:
+            await context.channel.send("Sorry but you are not an admin, message an admin to become an admin.")
+            print("failed to delete user:notadmin")
+
+
+    elif context.content.lower().split(" ")[0] == "delete_company!":
+        if context.author.name in admins:
+            if len(context.content.lower().split(" ")) == 2:
+                company = Company.query.filter_by(company_name=context.content.split(" ")[1]).first()
+                if company:
+                    for i in company.stocks:
+                        db.session.delete(i)
+                    db.session.delete(company)
+                    db.session.commit()
+                    await context.channel.send("I have deleted that company.")
+                    print("Succeeded to delete user:success")
+                else:
+                    await context.channel.send("Sorry but the company you entered does not exist.")
+                    print("failed to delete company:noexist")
+            else:
+                await context.channel.send("Sorry but the formmatting you used is wrong.")
+                print("failed to delete company:wrongformatt")
+        else:
+            await context.channel.send("Sorry but you are not an admin, message an admin to become an admin.")
+            print("failed to delete company:notadmin")
+
+    elif context.content.lower().split(" ")[0] == "add_admin!":
+        if context.author.name in admins:
+            if len(context.content.lower().split(" ")) == 2:
+                admins.append(context.content.split(" ")[1])
+                await context.channel.send("I have added that discord name to the list of admins.")
+                print("Succeeded to add admin:success")
+            else:
+                await context.channel.send("Sorry but the formmatting you used is wrong.")
+                print("failed to add admin:wrongformatt")
+        else:
+            await context.channel.send("Sorry but you are not an admin, message an admin to become an admin.")
+            print("failed to add admin:notadmin")
+
+client.run("NDg4MDIzNDA0NjE2NjEzOTAz.XZkR9w.Q62a4Q3L7nsNDj4nY4C8PZ4w9qI")
+#cd C:\Users\shakeandbakeforever\gitcolabstuff\colabstuff
